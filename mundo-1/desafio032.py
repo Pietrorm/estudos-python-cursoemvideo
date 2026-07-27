6546c6d089bd877e46963ae4b3dc7acb11f171e8
Pietrorm <loginsapppietro@gmail.com>
2026-07-27 16:30:28 -0300

    Desafio 032 Resolvido!!

--- mundo-1/desafio032.py
+++ mundo-1/desafio032.py
@@ -1,6 +1,6 @@
 ano = int(input('Digite qualquer ano para Análise: '))
 
-if ano % 4 == 0:
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('ANO BISSEXTO!!!')
 else:
     print('Não é um ano bissexto!!')
