513856fc1891ba360d4afab659a7754cd869f2b8
Pietrorm <loginsapppietro@gmail.com>
2026-08-19 16:51:20 -0300

    Adiciona classificando_atletas06.py

---
+++ mundo-2/classificando_atletas06.py
@@ -0,0 +1,16 @@
+from datetime import date
+nascimento = int(input('Ano de nascimento: '))
+idade = date.today().year - nascimento
+
+print(f'O atleta tem {idade} anos')
+
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade <= 19:
+    print('Classificação: JÚNIOR')
+elif idade <= 25:
+    print('Classificação: SÊNIOR')
+elif idade > 25:
+    print('Classificação: MASTER')
