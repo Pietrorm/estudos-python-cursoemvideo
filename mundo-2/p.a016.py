cbeeaa9d6b8836f8637b9df1bbd406ab22d9fbeb
Pietrorm <loginsapppietro@gmail.com>
2026-08-28 17:43:08 -0300

    Adicionando p.a016.py / Progressão Aritmética

---
+++ mundo-2/p.a016.py
@@ -0,0 +1,13 @@
+primeiro_termo = int(input('Digite o primeiro termo da progressão aritmética: '))
+razao = int(input('Digite a razão da P.A: '))
+
+for n in range(10):
+        an = primeiro_termo + n * razao
+        print(an, ' -> ', end=' ')
+print('ACABOU')
+        
+
+    
+        
+
+    
