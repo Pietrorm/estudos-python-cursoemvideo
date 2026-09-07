d847949f47364a3dc6f5a8432f5e65659dcac17c
Pietrorm <loginsapppietro@gmail.com>
2026-09-07 17:58:28 -0300

    Adicionando jogo_adivinhacao2.0_023.py

---
+++ exercicios/mundo-2/jogo_adivinhacao2.0_023.py
@@ -0,0 +1,26 @@
+from random import randint
+import time
+
+computador = randint(1,3)
+print(computador)
+
+palpites = 0
+acertou = False
+print('Jogo de Descobrir em qual número o SEU COMPUTADOR está pensando!!')
+
+while not acertou:
+    usuario = int(input('Seu palpite: '))
+    palpites += 1
+    print('Analisando..')
+    time.sleep(0.5)
+
+    if usuario == computador:
+        acertou = True
+    else:
+        if usuario < computador:
+            print('Mais...')
+        elif usuario > computador:
+            print('Menos...')
+
+print(f'Você Acertou com {palpites} tentativas')
+
