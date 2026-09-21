3f3dc3dcbe23a52c6822c879374dbf6c151022dd
Pietrorm <loginsapppietro@gmail.com>
2026-09-21 17:14:42 -0300

    Adicionando 3.0vtabuada032.py

---
+++ exercicios/mundo-2/3.0vtabuada032.py
@@ -0,0 +1,19 @@
+total = cont = tabuada = i = 0
+
+while True:
+    numero = int(input('Digite um número para saber sua tabuada: '))
+    if numero < 0:
+        print('Programa Encerrado!')
+        break
+    total += 1
+    cont = 0
+    for cont in range(1,11):
+        tabuada = numero * cont
+        print(f'{numero} x {cont} = {tabuada}')
+print(f'Você verificou a Tabuada de {total} números')
+
+'''while cont < 10:
+        cont += 1
+        tabuada = numero * cont
+        print(f'{numero} x {cont} = {tabuada}')
+print(f'Você verificou a Tabuada de {total} números') '''
