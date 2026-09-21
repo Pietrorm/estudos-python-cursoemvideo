0cdca424649dc652074f042b4d4dfeac7a1abe2d
Pietrorm <loginsapppietro@gmail.com>
2026-09-21 16:34:44 -0300

    Ctrl + S apenas

--- exercicios/mundo-2/maioremenor030.py
+++ exercicios/mundo-2/maioremenor030.py
@@ -3,18 +3,18 @@ cont = soma = media = maior = menor = 0
 resposta = ' '
 while resposta != 'N':
     num = int(input('Digite um número: '))
-    resposta = input('Quer continuar? [S/N] ').upper()
+    resposta = input('Quer continuar? [S/N] ').upper().strip()[0]
     soma += num
     cont += 1
     media = soma / cont
 
     if num == 1:
-        maior = num
-        menor = num
-    if num >= maior:
-        maior = num
-    if num < menor:
-        menor = num
+        maior = menor = num
+    else: 
+        if num >= maior:
+            maior = num
+        if num < menor:
+            menor = num
 
 print(f'Você digitou {cont} números e a média foi {(media):.2f}')
 print(f"""Maior valor: {maior}
